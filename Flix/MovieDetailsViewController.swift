ec0422b65d30d590cf51661d3e5ef75ba5253663
Keneisha Wiggan <keneishawiggan8@gmail.com>
2020-10-01 23:13:14 -0400

    Navigation and passing data

---
+++ Flix/MovieDetailsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MovieDetailsViewController.swift
+//  Flix
+//
+//  Created by Keneisha Wiggan on 9/30/20.
+//  Copyright © 2020 codePath. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailsViewController: UIViewController {
+
+    var movie: [String: Any]!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
